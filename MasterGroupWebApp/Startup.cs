3e2684365c8f061a3a726034d6a5a9820a63198f
Quese22 <MarqueseMhayes@gmail.com>
2019-10-14 15:39:15 -0400

    Add project files.

---
+++ MasterGroupWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MasterGroupWebApp.Startup))]
+namespace MasterGroupWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
